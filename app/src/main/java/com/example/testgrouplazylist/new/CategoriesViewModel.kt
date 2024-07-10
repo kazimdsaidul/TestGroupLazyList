589817130f876676bb2dede4e785b9ee134db25f
Kazi Md. Saidul <saidul.kazi@nexdecade.com>
2024-07-10 19:54:08 +0600

    version 1 static mutableStateListOf

--- app/src/main/java/com/example/testgrouplazylist/new/CategoriesViewModel.kt
+++ app/src/main/java/com/example/testgrouplazylist/new/CategoriesViewModel.kt
@@ -1,46 +1,27 @@
 package com.example.testgrouplazylist.new
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 
 class CategoriesViewModel : ViewModel() {
-    val categories = listOf(
-        Category(
-            id = 1,
-            name = "Pop Music",
-            playlists = listOf(
-                Playlist(
-                    1,
-                    "Top Hits",
-                    "Today's most popular songs",
-                    "https://images.toffeelive.com/images/program/300365/logo/web-logo/web-thumb-1717407841-84.png"
-                ),
-                Playlist(
-                    1,
-                    "Top Hits",
-                    "Today's most popular songs",
-                    "https://images.toffeelive.com/images/program/300365/logo/web-logo/web-thumb-1717407841-84.png"
-                ),
-                Playlist(
-                    1,
-                    "Top Hits",
-                    "Today's most popular songs",
-                    "https://images.toffeelive.com/images/program/300365/logo/web-logo/web-thumb-1717407841-84.png"
-                ),
+
+    val categories = mutableStateListOf<Category>().apply {
+        addAll(List(50) { categoryIndex ->
+            Category(
+                id = categoryIndex + 1,
+                name = "Category ${categoryIndex + 1}",
+                playlists = List(10) { playlistIndex ->
+                    Playlist(
+                        id = playlistIndex + 1,
+                        name = "Playlist ${playlistIndex + 1}",
+                        description = "Description for Playlist ${playlistIndex + 1} in Category ${categoryIndex + 1}",
+                        imageUrl = "https://images.toffeelive.com/images/program/300365/logo/web-logo/web-thumb-1717407841-84.png"
+                    )
+                }
             )
-        ),
-        Category(
-            id = 1,
-            name = "Rock Music",
-            playlists = listOf(
-                Playlist(
-                    1,
-                    "Rock Anthems",
-                    "Epic rock anthems",
-                    "https://images.toffeelive.com/images/program/300365/logo/web-logo/web-thumb-1717407841-84.png"
-                ),
-            )
-        )
-    )
+        })
+    }
+
 
     init {
         callAPIData();
